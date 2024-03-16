35d745ad969c477d318c6c29fadfaeceb354d73f
Software Engineer <96497987+KaharErbol@users.noreply.github.com>
2024-03-16 12:43:27 -0700

    Time: 529 ms (30.93%) | Memory: 34 MB (45.04%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,9 +1,17 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        res = []
-        tallest = -1
-        for i in range(len(heights)-1,-1,-1):
-            if heights[i] > tallest:
-                tallest = heights[i] 
-                res.append(i)
-        return sorted(res)
+        # res = []
+        # tallest = -1
+        # for i in range(len(heights)-1,-1,-1):
+        #     if heights[i] > tallest:
+        #         tallest = heights[i] 
+        #         res.append(i)
+        # return sorted(res)
+
+        stack = []
+        
+        for i in range(len(heights)):
+            while stack and heights[stack[-1]] <= heights[i]:
+                stack.pop()
+            stack.append(i)
+        return stack
