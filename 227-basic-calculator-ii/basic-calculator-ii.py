69df164363be9b7f8991be2609f7e742a7e29b0d
Software Engineer <96497987+KaharErbol@users.noreply.github.com>
2024-03-18 13:01:36 -0700

    Time: 99 ms (63.29%) | Memory: 19 MB (93.59%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,35 +1,24 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        curr_res = 0
+        length = len(s)
+        curr_num = 0
+        last_num = 0
         res = 0
-        num = 0
-        op = "+"  # keep the last operator we have seen
-        
-		# append a "+" sign at the end because we can catch the very last item
-        for ch in s + "+":
-            if ch.isdigit():
-                num = 10 * num + int(ch)
+        op = '+'
 
-            # if we have a symbol, we would start to calculate the previous part.
-            # note that we have to catch the last chracter since there will no sign afterwards to trigger calculation
-            if ch in ("+", "-", "*", "/"):
-                if op == "+":
-                    curr_res += num
-                elif op == "-":
-                    curr_res -= num
-                elif op == "*":
-                    curr_res *= num
-                elif op == "/":
-                    # in python if there is a negative number, we should alway use int() instead of //
-                    curr_res = int(curr_res / num)
-                
-                # if the chracter is "+" or "-", we do not need to worry about
-                # the priority so that we can add the curr_res to the eventual res
-                if ch in ("+", "-"):
-                    res += curr_res
-                    curr_res = 0
-                
-                op = ch
-                num = 0
-        
+        for i in range(length):
+            c = s[i]
+            if c.isdigit():
+                curr_num = curr_num * 10 + int(c)
+            if not c.isdigit() and c != ' ' or i == length - 1:
+                if op in ('+', '-'):
+                    res += last_num
+                    last_num = curr_num if op == '+' else -curr_num
+                elif op == '*':
+                    last_num *= curr_num
+                elif op == '/':
+                    last_num = int(last_num / curr_num)
+                op = c
+                curr_num = 0
+        res += last_num
         return res
