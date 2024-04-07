ef679d522644123d810066b5cc26f78f8a509a33
Software Engineer <96497987+KaharErbol@users.noreply.github.com>
2024-04-07 14:15:56 -0700

    Time: 40 ms (21.55%) | Memory: 16.6 MB (71.32%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,15 +1,19 @@
 class Solution:
-    def binaryExpo(self, x: float, n: int) -> float: 
+    def binaryExpo(self, x: float, n: int) -> float:
         if n == 0:
             return 1
-        
         if n < 0:
-            return 1.0 / self.binaryExpo(x, -1 * n)
+            return 1 / self.binaryExpo(x, -n)
 
-        if n % 2 == 1:
+        if n % 2 == 0:
+            return self.binaryExpo(x * x, n // 2)
+        
+        if n % 2 != 0:
             return x * self.binaryExpo(x * x, (n-1) // 2)
-        else:
-            return self.binaryExpo(x * x, n //2)
 
     def myPow(self, x: float, n: int) -> float:
         return self.binaryExpo(x, n)
+        
+       
+                
+
