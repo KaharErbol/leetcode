b9eb39856bb9f6d351d533c18d44b2075b7190fc
Software Engineer <96497987+KaharErbol@users.noreply.github.com>
2025-02-06 11:12:16 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (44.90%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Kahar
-        count = {}
-        res = 0
-        maxCount = 0
+        # count = {}
+        # res = 0
+        # maxCount = 0
+        # for n in nums:
+        #     count[n] = 1 + count.get(n, 0)
+        #     res = n if count[n] > maxCount else res
+        #     maxCount = max(count[n], maxCount)
+        # return res
+
+        # Kahar: Boyer-Moore Voting Algorithm -> Linea Time and O(1) Space
+        res, count = 0, 0
         for n in nums:
-            count[n] = 1 + count.get(n, 0)
-            res = n if count[n] > maxCount else res
-            maxCount = max(count[n], maxCount)
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
         return res
