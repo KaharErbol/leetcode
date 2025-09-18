b0da443273acfa23e5f53489d46634f7375ded80
Software Engineer <96497987+KaharErbol@users.noreply.github.com>
2025-09-18 07:17:09 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.25%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,11 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0 # It will stop at the elements equal to val
-
-        for i in range(len(nums)): # we check each element on i
-            if nums[i] != val: # when it's not val, meaning we will move the curr to k
-                nums[k] = nums[i] # because k will stop at elemtns equal to val
-                k += 1 # then go to the next one
-                # The condition is when the i element != val, 
-                # so k will move along when it's not val
+        k = 0
+        for i in range(len(nums)):
+          if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
         return k
